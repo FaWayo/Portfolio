4ff50dd9a1f7a17b9d3f8a9533a2fe64d82719f9
FaWayo <wayofatima@gmail.com>
2025-03-24 21:02:53 +0000

    webpack and tailwind css setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        './src/*.html',
+        './src/**/*.{html,js}'
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
